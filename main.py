6debaca54f6bddb178674e2090ba060cae7fb690
Julian <juli19cam@gmail.com>
2021-10-25 08:06:30 -0500

    structures of routes for main.py file

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
 from flask import Flask
+from flask import render_template
 import pydoc
+
+
+app = Flask(__name__)
+
+
+@route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route("/menuEstudiantes")
+def ingresar():
+    return render_template("menuEstudiantes.html")
+
+
+@app.route("/registro")
+def ingresar():
+    return render_template("registro.html")
+
+
+@app.route("/ingresar")
+def ingresar():
+    return render_template("ingresar.html")
+
+
+# Entry point
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
