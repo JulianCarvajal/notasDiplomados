9f41f1ec24151a0dd88781e46dea1ade5062b221
Julian <juli19cam@gmail.com>
2021-10-25 10:39:49 -0500

    Rename the static folder due a type error, modify the index.html to get the routes of external files with url_for() function

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 from flask import Flask
 from flask import render_template
+from flask import url_for
 import pydoc
 
 
 app = Flask(__name__)
 
 
-@route('/')
+@app.route("/")
 def index():
     return render_template("index.html")
 
 
 @app.route("/menuEstudiantes")
-def ingresar():
-    return render_template("menuEstudiantes.html")
+def menu():
+    return render_template("MenuEstudiantes.html")
 
 
 @app.route("/registro")
-def ingresar():
-    return render_template("registro.html")
+def resgistrar():
+    return render_template("Registro.html")
 
 
 @app.route("/ingresar")
 def ingresar():
-    return render_template("ingresar.html")
+    return render_template("Ingresar.html")
 
 
 # Entry point
